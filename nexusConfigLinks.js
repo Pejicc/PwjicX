b303bbb32696f321a7d5834b7e3fa5afe001c279
Pejicc <96324365+Pejicc@users.noreply.github.com>
2026-07-15 22:39:13 +0200

    Kreiraj linkove za Nexus Config - nexusconfig.nxc

---
+++ nexusConfigLinks.js
@@ -0,0 +1,27 @@
+// Nexus Config Links - Support i Provider
+const nexusConfigLinks = {
+  version: '1.0.0',
+  repository: 'https://github.com/Pejicc/PwjicX',
+  name: 'Nexus Config - nexusconfig.nxc',
+  
+  support: {
+    github: 'https://github.com/Pejicc/PwjicX/blob/main/nexusconfig.nxc',
+    raw: 'https://raw.githubusercontent.com/Pejicc/PwjicX/main/nexusconfig.nxc',
+    download: 'https://raw.githubusercontent.com/Pejicc/PwjicX/main/nexusconfig.nxc',
+    vsCode: 'vscode://file/https://github.com/Pejicc/PwjicX/blob/main/nexusconfig.nxc'
+  },
+  
+  provider: {
+    github: 'https://github.com/Pejicc/PwjicX/blob/main/provider.nxp',
+    raw: 'https://raw.githubusercontent.com/Pejicc/PwjicX/main/provider.nxp',
+    download: 'https://raw.githubusercontent.com/Pejicc/PwjicX/main/provider.nxp',
+    vsCode: 'vscode://file/https://github.com/Pejicc/PwjicX/blob/main/provider.nxp'
+  },
+  
+  directLinks: {
+    nexusconfigSupport: 'https://github.com/Pejicc/PwjicX/raw/main/nexusconfig.nxc',
+    providerSystem: 'https://github.com/Pejicc/PwjicX/raw/main/provider.nxp'
+  }
+};
+
+module.exports = nexusConfigLinks;
