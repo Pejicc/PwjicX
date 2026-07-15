881bf538985ee16297b92753d96640a6e78ad0d4
Pejicc <96324365+Pejicc@users.noreply.github.com>
2026-07-15 22:39:25 +0200

    Kreiraj linkove za Nexus Language Support - nexuslang.nxs

---
+++ nexusLangLinks.js
@@ -0,0 +1,27 @@
+// Nexus Language Support Links - Support i Provider
+const nexusLangLinks = {
+  version: '1.0.0',
+  repository: 'https://github.com/Pejicc/PwjicX',
+  name: 'Nexus Language Support - nexuslang.nxs',
+  
+  support: {
+    github: 'https://github.com/Pejicc/PwjicX/blob/main/nexuslang.nxs',
+    raw: 'https://raw.githubusercontent.com/Pejicc/PwjicX/main/nexuslang.nxs',
+    download: 'https://raw.githubusercontent.com/Pejicc/PwjicX/main/nexuslang.nxs',
+    vsCode: 'vscode://file/https://github.com/Pejicc/PwjicX/blob/main/nexuslang.nxs'
+  },
+  
+  provider: {
+    github: 'https://github.com/Pejicc/PwjicX/blob/main/provider.nxp',
+    raw: 'https://raw.githubusercontent.com/Pejicc/PwjicX/main/provider.nxp',
+    download: 'https://raw.githubusercontent.com/Pejicc/PwjicX/main/provider.nxp',
+    vsCode: 'vscode://file/https://github.com/Pejicc/PwjicX/blob/main/provider.nxp'
+  },
+  
+  directLinks: {
+    nexuslangSupport: 'https://github.com/Pejicc/PwjicX/raw/main/nexuslang.nxs',
+    providerSystem: 'https://github.com/Pejicc/PwjicX/raw/main/provider.nxp'
+  }
+};
+
+module.exports = nexusLangLinks;
